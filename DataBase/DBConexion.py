8659ec481b961fd2e0ef780eac68336234260701
CristopherSs <alanss2907@gmail.com>
2019-07-24 10:40:10 -0300

    conexion terminada

--- DataBase/DBConexion.py
+++ DataBase/DBConexion.py
@@ -7,18 +7,18 @@ class DB:
     def __init__(self) -> None:
         with open('./config.json') as file:
             data = json.load(file)
-        self.__connstr = data
+        self.__connstr = "host=%s port=%s user=%s password=%s dbname=%s" % (
+            data["host"], data["port"], data["user"], data["password"], data["database"])
 
-    def llamar_sp(self, store_procedure_name: str) -> bool:
+    def llamar_sp(self, store_procedure_name: str, values) -> bool:
         self.__conn = psycopg2.connect(self.__connstr)
         self.__cur = self.__conn.cursor()
-        self.__cur.execute(store_procedure_name)
+        self.__cur.callproc(store_procedure_name, values)
         try:
-            row = self.__cur.fetchone()
+            row = self.__cur.fetchall()
         except psycopg2.ProgrammingError:
             row = None
         finally:
-            self.__conn.commit()
             self.__cur.close()
             self.__conn.close()
         return row
