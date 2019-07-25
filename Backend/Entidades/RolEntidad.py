3e71171a0e7b6ecf055a85bbbf1970b19a3738d8
CristopherSs <alanss2907@gmail.com>
2019-07-25 20:47:23 -0300

    entidad rol modificada con la agregacion una nueva variable de permisos[]

--- Backend/Entidades/RolEntidad.py
+++ Backend/Entidades/RolEntidad.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass
 
+from Backend.Entidades import PermisoEntidad
+
 
 @dataclass
 class RolEntidad:
     rolId: int
     nombre: str
     descripcion: str
+    permisos: [PermisoEntidad]
