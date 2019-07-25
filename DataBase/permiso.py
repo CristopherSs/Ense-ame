0091cf2b31a685992bfef54b05e004ba41ca71ff
CristopherSs <alanss2907@gmail.com>
2019-07-24 22:01:55 -0300

    connecion con tabla permiso 80% terminado falta el de editar

--- DataBase/permiso.py
+++ DataBase/permiso.py
@@ -1,7 +1,6 @@
-from typing import List
+from typing import List, Union
 
 from Backend.Entidades.PermisoEntidad import PermisoEntidad
-from Backend.Entidades.RolEntidad import RolEntidad
 from DataBase.DBConexion import DB
 
 
@@ -11,22 +10,24 @@ class PermisoDB:
     def __init__(self) -> None:
         self.__DB = DB()
 
-    def guardar_permiso(self, nuevo_permiso_entidad: PermisoEntidad) -> int:
-        ...
+    def guardar_permiso(self, nuevo_permiso_entidad: PermisoEntidad) -> Union[None, int]:
+        return self.__DB.llamar_sp('guardarPermiso', [nuevo_permiso_entidad.nombre, nuevo_permiso_entidad.descripcion])
 
-    def obtener_permisos(self) -> None:
-        valores = self.__DB.llamar_sp('obtenerTodoPermisos', [])
-        lista_permisos = []
-        for datos in valores:
-            dato_unitario = \
-                {
-                    "permisoId": datos[0],
-                    "nombre": datos[1],
-                    "descripcion": datos[2],
-                }
-            lista_permisos.append(PermisoEntidad(**dato_unitario))
+    def obtener_permisos(self) -> Union[List, None]:
+        valores = self.__DB.llamar_sp('obtenerTodoPermisos', []) or []
+        if valores is not None:
+            lista_permisos = []
+            for datos in valores:
+                dato_unitario = \
+                    {
+                        "permisoId": datos[0],
+                        "nombre": datos[1],
+                        "descripcion": datos[2],
+                    }
+                lista_permisos.append(PermisoEntidad(**dato_unitario))
+            return lista_permisos
+        lista_permisos = valores
         return lista_permisos
 
-
-per = PermisoDB()
-per.obtener_permisos()
+    def eliminarPermiso(self, idPermiso: int) -> Union[int, None]:
+        return self.__DB.llamar_sp('eliminarPermiso', [idPermiso])
