059fa9e1d75bf9a6dacbaaf3ed436ec0dfe0d2d2
CristopherSs <alanss2907@gmail.com>
2019-07-25 00:23:14 -0300

    metodo mejorado de enrutamiento

--- FrontEnd/webui/src/app/app-routing.module.ts
+++ FrontEnd/webui/src/app/app-routing.module.ts
@@ -3,8 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { UsuarioComponent } from './usuario/usuario.component';
 import { RolesComponent } from './roles/roles.component';
 import { PermisosComponent } from './permisos/permisos.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
+  {
+    path:'',
+    component:AppComponent
+  },
   {
     path:'Usuarios',
     component:UsuarioComponent
